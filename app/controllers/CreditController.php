1eb1bd5024afe8998a8a8ab870a785d4ecdcac51
programacion <drawde@drawde.(none)>
2015-01-14 17:12:24 -0500

    tabla solicitud de credito

---
+++ app/controllers/CreditController.php
@@ -0,0 +1,39 @@
+<?php
+
+class CreditController extends BaseController
+{
+    public function index()
+    {
+        $tipo= [ "tipo de documento" => "Tipo de documento"]+[ 0 => "Cedula 0"]+[ 1 => "Cedula 1"];
+
+        return View::make('front.creditRequest',compact('tipo'));
+    }
+
+   /* private function validateForms($inputs = array())
+    {
+        $rules=array(
+            'cedula'
+            'ciudad_residencia'
+            'fecha_expedicion'
+            'fecha_nacimiento'
+            'celular'
+            'telefono'
+            'direccion'
+            'direccion_oficina'
+            'archivo'
+            'tipo_documento'
+            'referencia'
+            'valor_mensual'
+        )
+    }*/
+
+    private function getInputs($inputs = array())
+    {
+
+        foreach($inputs as $key => $val)
+        {
+            $inputs[$key] = $val;
+        }
+        return $inputs;
+    }
+}
--- app/routes.php
+++ app/routes.php
@@ -9,3 +9,6 @@ Route::post('login', ['as' => 'login', 'uses' => 'AuthController@login']);
 Route::get('sign-up', ['as' => 'sign-up', 'uses' => 'UserController@signUp']);
 Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
 
+//solicitud de credito
+Route::get('credito', ['as' => 'credito', 'uses' => 'CreditController@index']);
+Route::post('credito', ['as' => 'credito', 'uses' => 'CreditController@index']);
