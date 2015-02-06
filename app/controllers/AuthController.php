a9a6ffef5eb863be0d1d7b10e696e6da7eed97d3
iMac3 <imac3@drawde-2.local>
2015-02-06 17:43:56 -0500

    probando enviar correo

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -29,6 +29,7 @@ class AuthController extends BaseController
        // $success = $userRepo->passwordRestart(Input::get('email'));
         $password = str_random(30);
         $data = ['password' => $password];
+        $message='password es: '.$password;
         Mail::send('emails.password', $data, function ($message) {
             $message->subject('Restart password');
             $message->to('drawderiah@gmail.com');
