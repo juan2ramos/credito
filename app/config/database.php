efcc72cba5af27e4a67c60e08d95a14da3928f83
drawde <drawde@drawde>
2015-02-11 22:57:35 -0500

    quitando database

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'creditos',
+			'database'  => 'credito',
 			'username'  => 'root',
-			'password'  => 'root',
+			'password'  => '123456',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
