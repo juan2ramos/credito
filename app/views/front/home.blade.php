19d4585c1e57a7a9dfc50fb25d229784e98b8b27
iMac3 <imac3@drawde-2.local>
2015-02-23 17:07:30 -0500

    tamaño de la imagen slider

--- app/views/front/home.blade.php
+++ app/views/front/home.blade.php
@@ -20,16 +20,7 @@
         @endfor
             <span id="back"><</span>
             <span id="next">></span>
-      <!--  <img src="img/slider0.jpg">
-        <img src="img/slider1.jpg">
-        <img src="img/slider2.jpg">
-        <img src="img/slider3.jpg">
-        <span id="back"><</span>
-        <span id="next">></span>
-        <span  class="slider sliderValid">0</span>
-        <span  class="slider ">1</span>
-        <span  class="slider ">2</span>
-        <span  class="slider ">3</span>-->
+
     </div>
 @stop
 
