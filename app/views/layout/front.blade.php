acb0539e9ddacc5de92fd325522dc5a2e482ba43
chiricos <edwarddiaz92@gmail.com>
2015-03-10 14:48:40 -0500

    logo mi martinez

--- app/views/layout/front.blade.php
+++ app/views/layout/front.blade.php
@@ -87,7 +87,7 @@
 
     </div>
     <footer>
-        <p>®Lilipink Todos los derechos reservados - 2014 -  Diseño Web - Agencia de Publicidad <a href="http://mi-martinez.com" target="_blank" class="icon-logoblanco:before">Mi-martinez</a></p>
+        <p>®Lilipink Todos los derechos reservados - 2014 -  Diseño Web - Agencia de Publicidad <a href="http://mi-martinez.com" target="_blank" class="icon-logoblanco:before"><img src="{{asset('img/logomim.png')}}" style="width: 100px; opacity: 0.5;" alt=""/></a></p>
     </footer>
 
 
