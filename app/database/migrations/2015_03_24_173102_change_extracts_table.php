abdafca18c204d759b0bb0e7ae90f29a5f2874a7
chiricos <edwarddiaz92@gmail.com>
2015-03-24 17:39:05 -0500

    migration

--- app/database/migrations/2015_03_24_173102_change_extracts_table.php
+++ app/database/migrations/2015_03_24_173102_change_extracts_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeExtractsTable extends Migration {
+class ChangeExtractTable extends Migration {
 
 	/**
 	 * Run the migrations.
