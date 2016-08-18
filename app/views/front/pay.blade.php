354a8f549357e4d7c65ef64f147c1c4f6b6316fb
Juan2Ramos <juan2ramos>
2016-08-18 10:57:00 -0500

    cambios textos y responsive emprendedoras

--- app/views/front/pay.blade.php
+++ app/views/front/pay.blade.php
@@ -6,18 +6,54 @@
         <article class="tienda" >
             <img src="{{asset('img/tiendas.png')}}" alt="">
             <ul>
-                <li><span class="point"><img src="{{asset('img/point.png')}}" alt=""> </span><b>CONTADO: </b> Antes de facturar indica que eres una EMPRENDEDORA LILI PINK y presenta tu cédula</li>
-                <li><span class="point"><img src="{{asset('img/point.png')}}" alt=""> </span><b>CRÉDITO EMPRENDEDORA: </b>Si llenaste el formulario para crédito emprendedora y te fue aprobado un cupo, presenta tu tarjeta, cédula e indica que eres una EMPRENDEDORA LILI PINK, recuerda que el pago lo debes hacer en nuestras  tiendas a una (1) cuota</li>
+                <li><span class="point"><img src="{{asset('img/point.png')}}" alt=""> </span><b>CONTADO: </b> Antes de facturar indica
+
+                    que eres una EMPRENDEDORA LILI PINK
+
+                    y presenta tu cédula</li>
+                <li><span class="point"><img src="{{asset('img/point.png')}}" alt=""> </span><b>CRÉDITO EMPRENDEDORA: </b>
+                    Presenta
+
+                    tu tarjeta, cédula e indica que eres una
+
+                    EMPRENDEDORA LILI PINK.
+
+                    Recuerda que el pago lo debes hacer
+
+                    en un plazo de una (1) cuota - 15 días
+
+                    después de tu compra- en nuestras
+
+                    <span style="color: #BA007C">* TIENDAS LILI PINK :</span>indicando que vas
+
+                    a pagar la cuota de tu crédito
+
+                    emprendedora
+                </li>
             </ul>
         </article>
         <article>
             <img class="pedidos" src="{{asset('img/pedidos.png')}}" alt="">
             <ul>
                 <li><span class="point"><img src="{{asset('img/point.png')}}" alt=""> </span>Los pagos los puedes hacer en: <br>
-                    <span style="color: #BA007C">* BANCOLOMBIA:</span><br>
+                    <span style="color: #BA007C">* TIENDAS LILI PINK:</span><br>
+
+                    En las cajas indicas que vas a pagar un
+
+                    pedido hecho en línea del sistema
+
+                    emprendedoras <br>
                     Formato recaudo empresarial<br>
                     a nombre de Innova Quality S.A.S.<br>
                     Cuenta de ahorros 16862920931<br>
+                    <span style="color: #BA007C">* BANCOLOMBIA:</span><br>
+                    Formato recaudo empresarial
+
+                    a nombre de Innova Quality S.A.S.
+
+                    Cuenta de ahorros 16862920931
+
+                    Colocar el Referencia el número de pedido<br>
                      <span style="color: #BA007C">* EFECTY</span><br>
                     Convenio 110064<br>
                 </li>
