3c3c3c1ba316b868cf18ead0deba57b18b5986b6
santo52 <sanruiz1003@gmail.com>
2016-09-29 11:28:58 -0500

    views

--- app/views/emails/ECreditDelivery.blade.php
+++ app/views/emails/ECreditDelivery.blade.php
@@ -1,3 +1,3 @@
 <div class="image">
-    <img src="{{url('img/ECreditDelivery.jpg')}}" alt="enterpricing" style="width:100%; display:inline-block">
+    <img src="http://creditoslilipink.com/img/ECreditDelivery.jpg" alt="enterpricing" style="width:100%; display:inline-block">
 </div>
