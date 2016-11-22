6132889d506a43cdaf0b8e5028955fd7f5d436d7
Juan2Ramos <juan2ramos@gmail.com>
2016-11-22 08:10:50 -0500

    chat

--- app/views/layout/front.blade.php
+++ app/views/layout/front.blade.php
@@ -123,7 +123,17 @@
         <a id="cerrar" href="#">X</a>
     </div>
 </div>
-
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+    var _smartsupp = _smartsupp || {};
+    _smartsupp.key = 'b2fe573e2c5287ed5885c923481b93c16a88fae6';
+    window.smartsupp||(function(d) {
+        var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+        s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+        c.type='text/javascript';c.charset='utf-8';c.async=true;
+        c.src='//www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+    })(document);
+</script>
 </body>
 <!-- JavaScript -->
 
