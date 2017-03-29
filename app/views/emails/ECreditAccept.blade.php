3a8830834e6a968adaa6dec6e10290318ce3dbd7
juan2ramos <juan2ramos@gmail.com>
2017-03-29 09:33:43 -0500

    messages

--- app/views/emails/ECreditAccept.blade.php
+++ app/views/emails/ECreditAccept.blade.php
@@ -1,5 +1,4 @@
-<span style="font-size: 16px;display: block;">Su usuario es {{$email}}</span><br>
-<span style="font-size: 16px;display: block;">Su contraseña es {{$password}}</span><br>
+
 <div class="image">
     <img src="http://creditoslilipink.com/img/ECreditAccept.jpg" alt="enterpricing" style="width:100%; display:inline-block">
 </div>
