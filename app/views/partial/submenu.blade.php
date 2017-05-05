8a12ad12025d1a0ddd962ba4685bb2d1c3cee56d
Juan <juan2ramos@gmail.com>
2017-05-05 14:13:03 -0500

    Update submenu.blade.php

--- app/views/partial/submenu.blade.php
+++ app/views/partial/submenu.blade.php
@@ -3,7 +3,7 @@
         <li><a href="{{route('isEnterprising')}}">SE UNA EMPRENDEDORA</a></li>
         <li><a href="{{route('buy')}}">DONDE COMPRAR</a></li>
         <li><a href="{{route('pay')}}">COMO PAGAR</a></li>
-        <li><a href="http://emprendedoras.lilipink.com/">CATÁLOGO</a></li>
+        <li><a href="http://lilipink.com/shop">CATÁLOGO</a></li>
    {{--    <li><a href="http://emprendedoraslilipink.com/okonemovil/index.php" target="_blank">PEDIDO EN LINEA</a></li>--}}
     </ul>
 </div>
