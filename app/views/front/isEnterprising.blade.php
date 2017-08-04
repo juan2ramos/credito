e1ee69c3d5800a4e2bedb1b8a3c8d2a83e880dec
Juan <juan2ramos@gmail.com>
2017-08-04 10:34:03 -0500

    Update isEnterprising.blade.php

--- app/views/front/isEnterprising.blade.php
+++ app/views/front/isEnterprising.blade.php
@@ -4,22 +4,23 @@
     @include('partial.submenu')
     <section class="isEnterprising">
         <h2>QUÉ ES EL SISTEMA EMPRENDEDORA LILI PINK?</h2>
-        <p>Convirtiéndote en una EMPRENDEDORA LILI PINK podrás adquirir nuestros productos para tu propia red de ventas a través de un catálogo o nuestras tiendas con una  <span> ganancia del 25% </span>sobre el valor de tus pedidos</p>
+        <p>Convirtiéndote en una EMPRENDEDORA LILI PINK podrás adquirir nuestros productos para tu propia red de ventas a través de compra en tienda virtual o nuestras tiendas  seleccionadas con una  <span> ganancia del 25% </span>sobre el valor de tus pedidos</p>
         <h2>COMO FUNCIONA?</h2>
-        <p>Una vez diligencias el formulario de inscripción recibirás un e-mail de confirmación en un plazo de un día, luego podrás acceder a comprar en nuestro catálogo digital y en nuestras tiendas Lili Pink Emprendedoras con el beneficio del  <span>25% de ganancia</span> por compras minimas de $300.000</p>
+        <p>Una vez diligencias el formulario de inscripción recibirás un e-mail de confirmación en un plazo de un día, luego podrás acceder a comprar en  nuestra pagina web y en nuestras tiendas Lili Pink Emprendedoras con el beneficio del  <span>25% de ganancia</span> por compras minimas de $300.000</p>
         <h2>PAGOS DE CONTADO Y A CRÉDITO EMPRENDEDORA</h2>
         <p>Tenemos dos modalidades de pago, selecciona en el formulario la que más te convenga:<br>
-            * Contado: si compras en el catálogo virtual, debes pagar en efecty o bancolombia para hacerte el envio; si compras en tiendas Lili Pink Emprendedora, te presentas como emprendedora y haces tu pago para retirar los productos<br>
+            * Contado: si compras en la tienda virtual, debes pagar en efect, bancolombia o payu para hacerte el envio; si compras en tiendas Lili Pink Emprendedora, te presentas como emprendedora y haces tu pago para retirar los productos<br>
             * Crédito: tanto tus compras virtuales como en tiendas Lili Pink Emprendedora se difieren a 1 cuota para pago a 15 días en nuestras tiendas </p>
         <h2>A TENER EN CUENTA</h2>
         <p>
 
             * La inscripción es gratuita<br>
             * Puedes solicitar el crédito Lili Pink Emprendedora SIN CODEUDOR  <br>
-            * El catálogo virtual se actualiza mensualmente <br>
-            * El beneficio del 25% de dcto en catálogo virtual y tiendas Lili Pink Emprendedoras tienen como compra minima $300.000<br>
-            * El envio GRATUITO de tu pedido desde nuestro catálogo virtual tiene como compra minima $300.000, el resto de compras tienen un costo de envio de $10.000  <br>
-            * Una vez recibas tu pedido del catálogo virtual, revisa que haya llegado completo y en perfecto estado; de lo contrario debes notificar en las lineas que se encuentran  en la parte inferior en un plazo máximo de 1 día <br>
+            * La tienda virtual esta en constante actualización para brindarte lo más nuevo de nuestros productos
+            * Cuando hagas compras en linea con tu crédito emprendedora, debes notificar al whatsapp 310 575 6438 para que envien a tu correo el formato de autorización
+            * El beneficio del 25% de dcto en  tienda virtual y tiendas Lili Pink Emprendedoras tienen como compra minima $300.000<br>
+            * El envio GRATUITO de tu pedido desde nuestra tienda virtual  tiene como compra minima $300.000, el resto de compras tienen un costo de envio de $5.000 <br>
+            * Una vez recibas tu pedido de la tienda virtual, , revisa que haya llegado completo y en perfecto estado; de lo contrario debes notificar en las lineas que se encuentran  en la parte inferior en un plazo máximo de 1 día <br>
 
         </p>
         <h2>CAMBIOS Y DEVOLUCIONES</h2>
