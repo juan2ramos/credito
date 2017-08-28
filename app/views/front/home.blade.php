17d2f40a8f3a1ee48627a198011d3c0665da8e89
Juan <juan2ramos@gmail.com>
2017-08-28 10:43:06 -0500

    Update home.blade.php

--- app/views/front/home.blade.php
+++ app/views/front/home.blade.php
@@ -18,11 +18,12 @@
 
             @foreach($sliders as $key =>$slider)
             
-            <a href="{{ ($key == 0) ?"http://www.creditoslilipink.com/referidos":( ($key == 1)?"https://www.youtube.com/watch?v=b2AkFRyohT8":"#") }}
-            " >
-                <img class="img-slider" src="sliders/{{$slider['files']}}"/>
-            </a>
-            
+            @if($key == 0)
+ -                    <a href="https://www.youtube.com/watch?v=b2AkFRyohT8" target="_blank"><img class="img-slider" src="sliders/{{$slider['files']}}"/></a>
+ +                    <a href="http://www.creditoslilipink.com/referidos" ><img class="img-slider" src="sliders/{{$slider['files']}}"/></a>
+                  @else
+                      <img class="img-slider" src="sliders/{{$slider['files']}}"/>
+                  @endif
            
               
             @endforeach
