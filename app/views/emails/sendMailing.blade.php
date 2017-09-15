967d5bd755cad59cdb394b2f368e3911fa05e29c
juan2ramos <juan2ramos@gmail.com>
2017-09-15 11:38:58 -0500

    mailing

--- app/views/emails/sendMailing.blade.php
+++ app/views/emails/sendMailing.blade.php
@@ -8,26 +8,27 @@
 <body>
 <table width="600" border="0" align="center" cellpadding="0" cellspacing="0">
     <tr>
-        <td><a href="http://lilipink.com/" target="new"><img src="{{asset('logo-m.png')}}" width="600" height="94" alt="lilipink" /></a></td>
+        <td><a href="http://lilipink.com/" target="new"><img src="{{asset('img/logo-m.png')}}" width="600" height="94" alt="lilipink" /></a></td>
     </tr>
     <tr>
-        <td><a href="http://lilipink.com/" target="new"><img src="{{asset('categorias-m.png')}}" width="600" height="41" longdesc="http://www.lilipink.com" /></a></td>
+        <td><a href="http://lilipink.com/" target="new"><img src="{{asset('img/categorias-m.png')}}" width="600" height="41" longdesc="http://www.lilipink.com" /></a></td>
     </tr>
     <tr>
-        <td><img src="{{asset('gif-m.gif')}}" width="600" height="492" /></td>
+        <td><img src="{{asset('img/gif-m.gif')}}" width="600" height="492" /></td>
     </tr>
     <tr>
-        <td><img src="{{asset('info-m.png')}}" width="600" height="345" /></td>
+        <td><img src="{{asset('img/info-m.png')}}" width="600" height="345" /></td>
     </tr>
     <tr>
-        <td><img src="{{asset('esperamos-m.png')}}" width="600" height="177" /></td>
+        <td><img src="{{asset('img/esperamos-m.png')}}" width="600" height="177" /></td>
     </tr>
     <tr>
-        <td><a href="http://www.creditoslilipink.com/referidos" target="new"><img src="{{asset('terminos-m.png')}}" width="600" height="118" /></a></td>
+        <td><a href="http://www.creditoslilipink.com/referidos" target="new"><img src="{{asset('img/terminos-m.png')}}" width="600" height="118" /></a></td>
     </tr>
     <tr>
-        <td><a href="http://www.creditoslilipink.com/referidos" target="new"><img src="{{asset('tiendas-m.png')}}" width="600" height="64" /></a></td>
+        <td><a href="http://www.creditoslilipink.com/referidos" target="new"><img src="{{asset('img/tiendas-m.png')}}" width="600" height="64" /></a></td>
     </tr>
+    ok
 </table>
 </body>
 </html>
