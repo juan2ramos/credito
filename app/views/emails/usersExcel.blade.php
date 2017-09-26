1504507726f66f2f4d7154d60a8e5ed7a739abc6
Juan <juan2ramos@gmail.com>
2017-09-26 12:37:52 -0500

    Update usersExcel.blade.php

--- app/views/emails/usersExcel.blade.php
+++ app/views/emails/usersExcel.blade.php
@@ -1,2 +1,2 @@
 <p>Se ha generado el archivo de {{$doc}}</p>
-<p>Para descargar, haga <a href="http://creditoslilipink.com/exports/{{$doc}}">Click</a></p>
+<p>Para descargar, haga http://creditoslilipink.com/exports/{{$doc}} </p>
