f64b7b1d225a8df04c3f8f1c6bbed2c7846b17c9
juan2ramos <juan2ramos@gmail.com>
2017-10-30 13:59:59 -0500

    button delete accept credit poitn

--- app/views/front/request.blade.php
+++ app/views/front/request.blade.php
@@ -22,36 +22,43 @@
                 <th>Estado</th>
                 <th>Region</th>
                 <th>Punto</th>
-                <th>Revisar</th>
+                @if(Auth::user()->roles_id==1 || Auth::user()->roles_id==2)
+                    <th>Revisar</th>
+                @endif
             </tr>
             </thead>
             <tbody>
 
             @foreach($showRequest as $user)
-                 <tr>
-                     <td>{{$user->user_name}}</td>
-                     <td>{{$user->identification_card}}</td>
-                     <td>
-                         @if($user->priority == 0)
-                             Baja
-                         @elseif($user->priority == 1)
-                             Alta
-                         @elseif($user->priority == 2)
-                             Emp. Credito
-                         @endif
-                     </td>
-                     <td> Pendiente por aprobar </td>
-                     <td style="text-transform: capitalize;">
-                         @foreach($locations as $location)
-                             @if($user->location==$location->id)
-                                 {{$location->name}}
-                             @endif
-                         @endforeach
-                     </td>
-                     <td>{{$user->pointname}}</td>
-                     <td><a href="{{route('showCreditRequest', $user->id)}}" class="icon-folder-open"></a></td>
+                <tr>
+                    <td>{{$user->user_name}}</td>
+                    <td>{{$user->identification_card}}</td>
+                    <td>
+                        @if($user->priority == 0)
+                            Baja
+                        @elseif($user->priority == 1)
+                            Alta
+                        @elseif($user->priority == 2)
+                            Emp. Credito
+                        @endif
+                    </td>
+                    <td> Pendiente por aprobar</td>
+                    <td style="text-transform: capitalize;">
+                        @foreach($locations as $location)
+                            @if($user->location==$location->id)
+                                {{$location->name}}
+                            @endif
+                        @endforeach
+                    </td>
+                    <td>{{$user->pointname}}</td>
+                    <td>
+                        @if(Auth::user()->roles_id==1|| Auth::user()->roles_id==2)
+                            <a href="{{route('showCreditRequest', $user->id)}}" class="icon-folder-open"></a>
+                        @endif
 
-                 </tr>
+                    </td>
+
+                </tr>
             @endforeach
 
             @foreach($simpleEnterpricings as $user)
@@ -61,11 +68,19 @@
                     <td>
                         Emp. Contado
                     </td>
-                    <td> Pendiente por aprobar </td>
+                    <td> Pendiente por aprobar</td>
                     <td style="text-transform: capitalize;">{{$user->residency_city}}</td>
 
                     <td>{{$user->pointname}}</td>
-                    <td><a href="#" type="user" enable="{{route('admin.activate.user',$user->id)}}" disable="{{route('admin.destroy.user', $user->id)}}" route="{{route('getDataEnterpricing', $user->id)}}" class="icon-folder-open openPopup"></a></td>
+                    @if(Auth::user()->roles_id==1|| Auth::user()->roles_id==2)
+
+
+                        <td><a href="#" type="user" enable="{{route('admin.activate.user',$user->id)}}"
+                               disable="{{route('admin.destroy.user', $user->id)}}"
+                               route="{{route('getDataEnterpricing', $user->id)}}"
+                               class="icon-folder-open openPopup"></a>
+                        </td>
+                    @endif
                 </tr>
             @endforeach
             </tbody>
