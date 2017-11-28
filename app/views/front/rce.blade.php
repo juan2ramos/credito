8857212749c404cf90b420340b510f663787887a
Juan <juan2ramos@gmail.com>
2017-11-28 14:17:35 -0500

    Create rce.blade.php

---
+++ app/views/front/rce.blade.php
@@ -0,0 +1,94 @@
+@extends('layout/front')
+
+@section('content')
+@include('partial.submenu')
+<div  class="container is-text-center ">
+<header>
+<img src="https://lh3.googleusercontent.com/MhuSbkEqZBLCj92fbD-348RAT8wygIDfxeN658jmBszVfcN2snS93eI1718IAmW7ZM7r_jZLWr7uLv7pwQRuFIVUNXPpj-304yjtbjByVf7hxIntO5pTlcoJ39PC3vCaW5J22BcZNur3v0loSdtpmCHwA6i_Za9np935P2MkpMLlINVHm6XOlZ7UVcTRcbUgwgNMrXwzcDYKD8BtKCkyKXSgQ8DczKL8zXOB_NjJ_8yBYqnuOxYg7d9oMYyM4CgJqvUafIK4ofmUNeMevZ5mgDBmfVzOWQqUqrdZ4vHsxTvCIbJguD-gTDZ7CJBk7uhFe15AX7fMh21PRsHXnbCr2Rktp6jUClYLYMG2mq6_34R13Npztruv1X9lUZ5Ah5F3syuF4gmTrnOqu60TIhw__AHELjy88AeFqF-GZKL_MYWrjy6o0Ka04wu8x5QsC8KGGfFycXhlKuMALJ2ywDSnBC_DMM2YUhVMiOgo0173Lcd4_ROWfmztuVXoWwDKJVIEHwcboPx6BaqvcgOOnYQY34L5eHE0SEPB0mRaqJ6iNxd5k6ByJTWnWU6LpDBwmusWs_6dxD1uAuNcmf8f4ODRx3TekIUKeyygD1PKQnf_9t8=w800-h400-no" alt="">
+	</header>
+	<div class="table-container ">
+<table> 
+	<thead>
+		<tr>
+		<th>CIUDAD</th>
+		<th>DEPARTAMENTO</th>
+		<th>CIUDAD</th>
+		<th>DEPARTAMENTO</th>
+			</tr>
+	</thead>
+	<tbody>
+			<tr><td>Acapulco (Stder.)</td> <td>Santander <td>La Calera (Cund)</td> <td>Cundinamarca</td>
+<tr> <td>Amaime (Val)</td> <td>Valle </td> <td>La Ceja (Ant) </td><td>Antioquia</td></tr>
+<tr><td>Andalucia (Val)</td><td> Valle</td><td> La Estrella (Ant)</td><td> Antioquia</td></tr>
+<tr><td>Armenia (Qui)</td><td> Quindio</td><td> La Gran Via (Mag)<td> Magdalena</td></tr>
+<tr><td>Barbosa (Ant)</td><td> Antioquia</td><td> La Tebaida (Qui)</td><td> Quindio</td></tr>
+<tr><td>Barranquilla (Atl)</td><td> Atlantico</td><td> Lorica (Cor)</td><td> Cordoba</td></tr>
+<tr><td>Bello (Ant)</td><td> Antioquia</td><td> Los Patios (N_Stder)</td><td> Norte de Santander</tr>
+<tr><td>Bogota (Cund)</td><td> Cundinamarca</td><td> Madrid (Cund)</td><td> Cundinamarca</tr>
+<tr><td>Bosconia (Ces) </td><td>Cesar</td><td> Malambo (Atl)</td><td> Atlantico</tr>
+<tr><td>Briceño (Cund)</td><td> Cundinamarca</td><td> Manizales (Cal)</td><td> Caldas</tr>
+<tr><td>Bucaramanga (Stder)</td><td> Santander</td><td> Marinilla (Ant)</td><td> Antioquia</tr>
+<tr><td>Buenaventura (Val)</td><td> Valle </td><td>Medellin (Ant)</td><td> Antioquia</tr>
+<tr><td>Buenos Aires (Tol)</td><td> Tolima</td><td> Melgar (Tol)</td><td> Tolima</tr>
+<tr><td>Buga La Grande (Val)</td><td> Valle</td><td> Miranda (Cau)</td><td> Cauca</tr>
+<tr><td>Buga (Val)</td><td> Valle</td><td> Montenegro (Qui)</td><td> Quindio</tr>
+<tr><td>Cajica (Cund)</td><td> Cundinamarca</td><td> Monteria (Cor)</td><td> Cordoba</tr>
+<tr><td>Calarca (Qui)</td><td> Quindio</td><td> Morroa (Suc)</td><td> Sucre</tr>
+<tr><td>Cali (Val)</td><td> Valle</td><td> Neiva (Hui)</td><td> Huila</tr>
+<tr><td>Caloto (Cau)</td><td> Cauca</td><td> Paipa (Boy)</td><td> Boyaca</tr>
+<tr><td>Candelaria (Val)</td><td> Valle</td><td> Palmira (Val)</td><td> Valle</tr>
+<tr><td>Cartagena (Bol)</td><td> Bolivar</td><td> Pasto (Nar)</td><td> Nariño</tr>
+<tr><td>Cartago (Val)</td><td> Valle</td><td> Pereira (Ris)</td><td> Risaralda</tr>
+<tr><td>Cerete (Cor)</td><td> Cordoba</td><td> Piedecuesta (Stder)</td><td> Santander </tr>
+<tr><td>Chia (Cund)</td><td> Cundinamarca</td><td> Popayan (Cau)</td><td> Cauca</tr>
+<tr><td>Chicoral (Tol)</td><td> Tolima</td><td> Pradera (Val)</td><td> Valle</td></tr>
+<tr><td>Chinchina (Cal)</td><td> Caldas</td><td> Puerto Tejada (Cau)</td><td> Cauca</tr>
+<tr><td>Chiquinquira (Boy)</td><td> Boyaca</td><td> Quimbaya (Qui)</td><td> Quindio</tr>
+<tr><td>Cienaga de Oro (Cor)</td><td> Cordoba</td><td> Ricaurte (Cund)</td><td> Cundinamarca</tr>
+<tr><td>Cienaga (Mag)</td><td> Magdalena</td><td> Rionegro (Ant)</td><td> Antioquia</tr>
+<tr><td>Circasia (Qui)</td><td> Quindio</td><td> Rozo (Val)</td><td> Valle</tr>
+<tr><td>Copacabana (Ant)</td><td> Antioquia</td><td> Sabaneta (Ant)</td><td> Antioquia</tr>
+<tr><td>Corozal (Suc)</td><td> Sucre</td><td> Sahagun (Cor)</td><td> Cordoba</tr>
+<tr><td>Cota (Cund)</td><td> Cundinamarca</td><td> Sampues (Suc)</td><td> Sucre</tr>
+<tr><td>Coveñas (Suc)</td><td> Sucre</td><td> San Antonio de Prado (Ant)</td><td> Antioquia</tr>
+<tr><td>Cucuta (N_Stder)</td><td> Norte de Santander</td><td> San Pedro (Suc)</td><td> Sucre</tr>
+<tr><td>Dosquebradas (Ris)</td><td> Risaralda</td><td> Santa Marta (Mag)</td><td> Magdalena</tr>
+<tr><td>Duitama (Boy)</td><td> Boyaca</td><td> Santander de Quilichao (Cau)</td><td> Cauca</tr>
+<tr><td>El Carmelo (Val)</td><td> Valle</td><td> Santa Rosa de Cabal (Ris)</td><td> Risaralda</tr>
+<tr><td>El Carmen de Viboral (Ant)</td><td> Antioquia</td><td> Santuario (Ant)</td><td> Antioquia</tr>
+<tr><td>El Cerrito (Val)</td><td> Val</td><td> Sibate (Cund)</td><td> Cundinamarca</tr>
+<tr><td>El Placer (Val)</td><td> Valle</td><td> Sincelejo (Suc)</td><td> Sucre</tr>
+<tr><td>El Retiro (Ant)</td><td> Antioquia</td><td> Soacha (Cund)</td><td> Cundinamarca</tr>
+<tr><td>Envigado (Ant)</td><td> Antioquia</td><td> Sogamoso (Boy) </td><td>Boyaca</tr>
+<tr><td>spinal (Tol)</td><td> Tolima</td><td> Soledad (Atl)</td><td> Atlantico</tr>
+<tr><td>Facatativa (Cund)</td><td> Cundinamarca</td><td> Sonso (Val)</td><td> Valle</tr>
+<tr><td>Flandes (Tol)</td><td> Tolima</td><td> Sopo (Cund)</td><td> Cundinamarca</tr>
+<tr><td>Floridablanca (Stder)</td><td> Santander</td><td> Tabio (Cund)</td><td> Cundinamarca</tr>
+<tr><td>Florida (Valle)</td><td> Valle</td><td> Taganga (Mag)</td><td> Magdalena</tr>
+<tr><td>Fundacion (Mag)</td><td> Magdalena</td><td> Tenjo (Cund)</td><td> Cundinamarca</tr>
+<tr><td>Funza (Cund)</td><td> Cundinamarca</td><td> Tibasosa (Boy) </td><td>Boyaca</tr>
+<tr><td>Fusagasuga (Cund)</td><td> Cundinamarca</td><td> Tocancipa (Cund)</td><td> Cundinamarca</tr>
+<tr><td>Gachancipa (Cund)</td><td> Cundinamarca</td><td> Tulua (Val)</td><td> Valle</tr>
+<tr><td>Gaira (Mag)</td><td> Magdalena</td><td> Tunja (Boy)</td><td> Boyaca</tr>
+<tr><td>Galapa (Atl)</td><td> Atlantico</td><td> Turbaco (Bol)</td><td> Bolivar</tr>
+<tr><td>Ginebra (Val)</td><td> Valle</td><td> Ubate (Cund)</td><td> Cundinamarca</tr>
+<tr><td>Girardota (Ant)</td><td> Antioquia</td><td> Valledupar (Ces)</td><td> Cesar</tr>
+<tr><td>Girardot (Cund)</td><td> Cundinamarca</td><td> Vijes (Val)</td><td> Valle</tr>
+<tr><td>Giron (Stder)</td><td> Santander</td><td> Villa del Rosario (N_Stder)</td><td> Norte de Santander</tr>
+<tr><td>Guacari (Val)</td><td> Valle</td><td> Villa Gorgona (Val)</td><td> Valle</tr>
+<tr><td>Gualanday (Tol)</td><td> Tolima</td><td> Villamaria (Cal)</td><td> Caldas</tr>
+<tr><td>Guarne (Ant)</td><td> Antioquia</td><td> Villa Rica (Cau)</td><td> Cauca</tr>
+<tr><td>Ibague (Tol)</td><td> Tolima</td><td> Villavicencio (Met)</td><td> Meta</tr>
+<tr><td>Ipiales (Nar)</td><td> Nariño</td><td> Yopal (Cas)</td><td> Casanare</tr>
+<tr><td>Itagui (Ant)</td><td> Antioquia</td><td> Yumbo (Val)</td><td> Valle</tr>
+<tr><td>Jamundi (Val)</td><td> Valle</td><td> Zipaquira (Cund)</td><td> Cundinamarca</tr>
+<tr><td>La Boquilla (Bol)</td><td> Bolivar</tr>
+
+	</tbody>
+</table >
+		</div>
+	</div>
+@stop
+@section('styles')
+   <link rel="stylesheet prefetch" href="https://unpkg.com/inuit-css">
+@stop
